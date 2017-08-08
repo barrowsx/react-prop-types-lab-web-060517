721647e6a3a08f486886e67905060a56de52df30
barrowsx <wwboyer14@mail.strakejesuit.org>
2017-08-08 10:53:47 -0400

    𝔠𝔬𝔪𝔪𝔦𝔱𝔱𝔢𝔡 𝔲𝔰𝔦𝔫𝔤 𝔞𝔯𝔠𝔞𝔫𝔢 𝔪𝔞𝔤𝔦𝔠𝔨𝔰 𝔩𝔬𝔰𝔱 𝔱𝔬 𝔱𝔥𝔦𝔰 𝔪𝔬𝔯𝔱𝔞𝔩 𝔠𝔬𝔦𝔩

--- src/components/Product.js
+++ src/components/Product.js
@@ -1,1 +1,55 @@
 // Code Product Component Here
+import React from 'react';
+import PropTypes from 'prop-types';
+
+class Product extends React.Component{
+  render(){
+    return (
+      <div className="product">
+        <ul>
+          <li>{this.props.name}</li>
+          <li>{this.props.producer}</li>
+          <li>{this.props.hasWatermark}</li>
+          <li>{this.props.color}</li>
+          <li>{this.props.weight}</li>
+        </ul>
+      </div>
+    )
+  }
+}
+
+function createCustomPropType(isRequired){
+  return function(props, propName, componentName){
+    if(!props[propName]){
+      if(isRequired){
+        throw new Error(
+          'xd kid'
+        )
+      }
+    }
+    if(typeof(props[propName]) !== 'number'){
+      return new Error(
+        'ur actually so bad'
+      )
+    }
+    if((props[propName] > 300) || (props[propName] < 80)){
+      return new Error(
+        'LMAO'
+      )
+    }
+  }
+}
+
+Product.defaultProps = {
+  hasWatermark: false
+}
+
+Product.propTypes = {
+  name: PropTypes.string.isRequired,
+  producer: PropTypes.string,
+  hasWatermark: PropTypes.bool,
+  color: PropTypes.oneOf(['white', 'eggshell-white', 'salmon']).isRequired,
+  weight: createCustomPropType(true)
+}
+
+export default Product
